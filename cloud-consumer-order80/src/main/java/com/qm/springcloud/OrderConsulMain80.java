a3690fc369b8229c5261c3c4a2c10614ec9f5e51
邱猛 <qiumeng@ansosz.com>
2020-07-28 17:13:57 +0800

    消费者模块提交

---
+++ cloud-consumer-order80/src/main/java/com/qm/springcloud/OrderConsulMain80.java
@@ -0,0 +1,18 @@
+package com.qm.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author qiumeng
+ * @version 1.0
+ * @description
+ * @date 2020/7/28 16:32
+ */
+@SpringBootApplication
+public class OrderConsulMain80 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderConsulMain80.class, args);
+    }
+}

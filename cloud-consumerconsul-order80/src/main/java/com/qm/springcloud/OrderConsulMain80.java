f2179ccd9a8d41e4f2ba5b5ea57cc3a9841c93bb
邱猛 <qiumeng@ansosz.com>
2020-07-29 16:01:22 +0800

    consul

---
+++ cloud-consumerconsul-order80/src/main/java/com/qm/springcloud/OrderConsulMain80.java
@@ -0,0 +1,20 @@
+package com.qm.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author qiumeng
+ * @version 1.0
+ * @description
+ * @date 2020/7/29 15:45
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class OrderConsulMain80 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderConsulMain80.class,args);
+    }
+}
---
+++ cloud-providerconsul-payment8006/src/main/java/com/qm/springcloud/PaymentConsul8006.java
@@ -0,0 +1,20 @@
+package com.qm.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author qiumeng
+ * @version 1.0
+ * @description
+ * @date 2020/7/29 15:34
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PaymentConsul8006 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentConsul8006.class,args);
+    }
+}

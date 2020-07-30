22863d3292c3a69833d2c63d08acba40e3844e20
qium <774697399@qq.com>
2020-07-30 21:00:40 +0800

    OpenFeign

---
+++ cloud-consumer-feign-order80/src/main/java/com/qm/springcloud/service/PaymentFeignService.java
@@ -0,0 +1,25 @@
+package com.qm.springcloud.service;
+
+import com.qm.springcloud.CommonResult;
+import com.qm.springcloud.entities.Payment;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+/**
+ * @author qiumeng
+ * @version 1.0
+ * @description
+ * @date 2020/7/30 20:23
+ */
+@Component
+@FeignClient(value = "CLOUD-PAYMENT-SERVICE")
+public interface PaymentFeignService {
+
+    @GetMapping(value="/payment/get/{id}")
+    public CommonResult<Payment> getPaymentById(@PathVariable("id") Long id);
+
+    @GetMapping(value = "/payment/feign/timeout")
+    public String paymentFeignTimeout();
+}

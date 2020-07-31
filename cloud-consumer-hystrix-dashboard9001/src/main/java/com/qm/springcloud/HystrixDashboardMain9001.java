180021a0ea3fd3b5c3b548debd5e535337493704
邱猛 <qiumeng@ansosz.com>
2020-07-31 16:58:53 +0800

    hystrix dashboard

---
+++ cloud-consumer-hystrix-dashboard9001/src/main/java/com/qm/springcloud/HystrixDashboardMain9001.java
@@ -0,0 +1,20 @@
+package com.qm.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * @author qiumeng
+ * @version 1.0
+ * @description
+ * @date 2020/7/31 16:50
+ */
+@SpringBootApplication
+@EnableHystrixDashboard
+public class HystrixDashboardMain9001 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboardMain9001.class,args);
+    }
+}

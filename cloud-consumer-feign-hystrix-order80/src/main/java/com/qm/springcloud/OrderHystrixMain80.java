167e14f77421e5972394e409af85a86b051f146b
邱猛 <qiumeng@ansosz.com>
2020-07-31 16:47:24 +0800

    hystrix

---
+++ cloud-consumer-feign-hystrix-order80/src/main/java/com/qm/springcloud/OrderHystrixMain80.java
@@ -0,0 +1,22 @@
+package com.qm.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * @author qiumeng
+ * @version 1.0
+ * @description
+ * @date 2020/7/31 10:16
+ */
+@SpringBootApplication
+@EnableFeignClients
+@EnableHystrix
+public class OrderHystrixMain80 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderHystrixMain80.class,args);
+    }
+}
---
+++ cloud-provider-hystrix-payment8001/src/main/java/com/qm/springcloud/PaymentHystrixMain8001.java
@@ -0,0 +1,23 @@
+package com.qm.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author qiumeng
+ * @version 1.0
+ * @description
+ * @date 2020/7/31 9:37
+ */
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableCircuitBreaker
+public class PaymentHystrixMain8001 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentHystrixMain8001.class,args);
+    }
+}

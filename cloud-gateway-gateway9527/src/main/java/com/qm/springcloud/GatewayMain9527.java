eb0fb010606e03dcacf692ea7d37f69aaf95ff78
qium <774697399@qq.com>
2020-08-02 20:45:59 +0800

    GateWay

---
+++ cloud-gateway-gateway9527/src/main/java/com/qm/springcloud/GatewayMain9527.java
@@ -0,0 +1,20 @@
+package com.qm.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author qiumeng
+ * @version 1.0
+ * @description
+ * @date 2020/8/2 20:22
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class GatewayMain9527 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayMain9527.class,args);
+    }
+}
